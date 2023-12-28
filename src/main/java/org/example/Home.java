73f608d37afbcc422f9dd14e6f641b4081584b73
ScalaDanilo <danilo.scala@studenti.unina.it>
2023-12-28 17:32:52 +0100

    Prova

--- src/main/java/org/example/Home.java
+++ src/main/java/org/example/Home.java
@@ -8,13 +8,19 @@ import java.io.File;
 import java.io.IOException;
 
 public class Home{
-    BufferedImage bufferedImage = ImageIO.read(new File("C:\\Users\\Danilo\\Desktop\\Secondo anno\\Progetto OO e BD\\OO\\ProgettoTraccia3_OO_e_BD\\Logo1.jpeg"));
-    Image image = bufferedImage.getScaledInstance(200, 200, Image.SCALE_DEFAULT);
-    ImageIcon icon = new ImageIcon(image);
+    BufferedImage bufferedImage1 = ImageIO.read(new File("C:\\Users\\Danilo\\Desktop\\Secondo anno\\Progetto OO e BD\\OO\\ProgettoTraccia3_OO_e_BD\\Logo1.jpeg"));
+    Image image1 = bufferedImage1.getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+    ImageIcon icon1 = new ImageIcon(image1);
+    BufferedImage bufferedImage2 = ImageIO.read(new File("C:\\Users\\Danilo\\Desktop\\Secondo anno\\Progetto OO e BD\\OO\\ProgettoTraccia3_OO_e_BD\\university.png"));
+    Image image2 = bufferedImage2.getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+    ImageIcon icon2 = new ImageIcon(image2);
     private final JFrame home;
     private JPanel panelHome;
     private JPanel panelLogo;
-    private JLabel label1;
+    private JLabel logo;
+    private JLabel f2;
+    private JPanel panelF2;
+
 
     public Home() throws IOException {
         home = new JFrame("Home");
@@ -22,11 +28,17 @@ public class Home{
         home.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         home.pack();
         home.setMinimumSize(new Dimension(800,500));
-        home.setLayout(null);
-        panelLogo.setSize(600,600);
-        panelLogo.setBounds(0,0,600,600);
+        home.add(logo);
+        home.add(f2);
 
-        label1.setIcon(icon);
+        logo.setVerticalAlignment(SwingConstants.TOP);
+        logo.setHorizontalAlignment(SwingConstants.LEFT);
+
+        f2.setVerticalAlignment(SwingConstants.TOP);
+        f2.setHorizontalAlignment(SwingConstants.RIGHT);
+
+        logo.setIcon(icon1);
+        f2.setIcon(icon2);
     }
 
     public void initHome(){home.setVisible(true);}
